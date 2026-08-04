d089d21a83517dc233a0dd4df648f0d907b9c0b2
TheShivaji <shivajijagdale2005@gmail.com>
2026-08-04 17:01:06 +0530

    feat: added search filters for deleted artists, fixed album search, and implemented playlist search

--- Backend/src/controllers/search.controller.ts
+++ Backend/src/controllers/search.controller.ts
@@ -2,6 +2,7 @@ import { prisma } from "../config/db.js";
 import type { AuthRequest } from "../middleware/auth.middleware.js";
 import type { Response } from "express";
 import { Prisma } from "@prisma/client";
+import { title } from "node:process";
 
 const validation = async function (res: Response, req: AuthRequest) {
     const { q, page = 1, limit = 20 } = req.query;
@@ -29,7 +30,6 @@ const validation = async function (res: Response, req: AuthRequest) {
         limit: Number(limit),
     }
 }
-
 
 export const searchArtists = async (req: AuthRequest, res: Response) => {
     try {
@@ -89,6 +89,12 @@ export const searchSong = async function (req: AuthRequest, res: Response) {
         if (!searchParams) return;
         const { searchQuery, skip, take, page, limit } = searchParams;
         const where: Prisma.SongWhereInput = {
+            // Ensure no artist on the song is deleted
+            artists: {
+                none: {
+                    isDeleted: true
+                }
+            },
             OR: [
                 {
                     title: {
@@ -102,7 +108,9 @@ export const searchSong = async function (req: AuthRequest, res: Response) {
                             name: {
                                 contains: searchQuery,
                                 mode: "insensitive"
-                            }
+                            },
+                            // Ensure the searched artist is also not deleted
+                            isDeleted: false
                         }
                     }
                 }
@@ -139,6 +147,131 @@ export const searchSong = async function (req: AuthRequest, res: Response) {
             }
         })
 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const searchAlbum = async (req: AuthRequest, res: Response) => {
+    try {
+        const searchParams = await validation(res, req)
+        if (!searchParams) return
+        const { searchQuery, take, page, limit, skip } = searchParams
+
+        const where: Prisma.AlbumWhereInput = {
+            artists: {
+                none: {
+                    isDeleted: true
+                }
+            },
+            OR: [
+                {
+                    title: {
+                        contains: searchQuery,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    artists: {
+                        some: {
+                            name: {
+                                contains: searchQuery,
+                                mode: "insensitive"
+                            },
+                            // Ensure the searched artist is also not deleted
+                            isDeleted: false
+                        }
+                    }
+                }
+            ]
+        }
+
+        const album = await prisma.album.findMany({
+            where,
+            skip,
+            take
+        })
+
+        if (album.length === 0) {
+            return res.status(404).json({
+                message: "Albums Not Founded"
+            })
+        }
+
+        const totalAlbum = await prisma.album.count({
+            where
+        })
+
+        return res.status(200).json({
+            message: "album founded",
+            album,
+            pagination: {
+                total: Number(totalAlbum),
+                page,
+                limit,
+                totalPages: Math.ceil(totalAlbum / limit)
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const searchPlaylist = async (req: AuthRequest, res: Response) => {
+    try {
+        const searchParams = await validation(res, req)
+        if (!searchParams) return
+        const { searchQuery, take, page, limit, skip } = searchParams
+
+        const where: Prisma.PlaylistWhereInput = {
+            isPublic: true, 
+            OR: [
+                {
+                    name: {
+                        contains: searchQuery,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    description: {
+                        contains: searchQuery,
+                        mode: "insensitive"
+                    }
+                }
+            ]
+        }
+
+        const playlist = await prisma.playlist.findMany({
+            where,
+            skip,
+            take,
+            orderBy: {
+                name: "asc"
+            }
+        })
+
+        if (playlist.length === 0) {
+            return res.status(404).json({
+                message: "Playlists Not Founded"
+            })
+        }
+
+        const totalPlaylist = await prisma.playlist.count({
+            where
+        })
+
+        return res.status(200).json({
+            message: "playlist founded",
+            playlist,
+            pagination: {
+                total: Number(totalPlaylist),
+                page,
+                limit,
+                totalPages: Math.ceil(totalPlaylist / limit)
+            }
+        })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Internal server error" });
